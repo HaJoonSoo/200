e597043df5cbbb90d008a57c0963377c0446dd21
HaJoonSoo <gkwnstn95@gmail.com>
2021-10-15 21:12:58 +0900

    두 날짜 사이의 경과시간 계산

---
+++ 115.js
@@ -0,0 +1,27 @@
+// 두 개의 날짜 사이의 경과 시간 계산 하기
+
+Date.daysDiff = (date1, date2) => {
+    if(!(date1 instanceof Date) || !(date2 instanceof Date)) return '';
+
+    const d1 = date1.getTime();
+    const d2 = date2.getTime();
+
+    let diff = d2 - d1;
+
+    const second = Math.floor((diff = diff / 1000) % 60);
+    const minutes = Math.floor((diff = diff / 60) % 60);
+    const hours = Math.floor((diff = diff / 60) % 24);
+    const days = Math.floor(diff / 24);
+    return `${days} days, ${hours} hours, ${minutes} minutes , and ${second} second`;
+}
+
+var from = new Date(2000, 0, 1);
+var to = new Date(from.getFullYear() + 1, from.getMonth() + 3,
+from .getDate() + 5, from.getHours() + 4, from.getMinutes() + 30,
+from.getSeconds() + 50);
+
+console.log(`From   >${from}`)
+console.log(`To     >${to}`)
+console.log(Date.daysDiff(from, to));
+
+// date1과 date2 중 하나라도 Date 객체 인스턴스가 아닌 경우, 빈 문자열을 반환하며 함수를 종료시킨다
